cd5fa4a56a46d525914c8b4852f3ca9cbf6e0cbb
Abdulrafeh-Knarkz <abdul.rafeh@knarkz.co.uk>
2023-02-14 15:05:38 +0500

    first commit

---
+++ example/android/app/src/main/kotlin/com/example/dialogs_x/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dialogs_x.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/dialogs_x.dart
@@ -0,0 +1,13 @@
+library dialogs_x;
+
+export 'package:dialogs_x/src/data/progress_data_model.dart';
+export 'package:dialogs_x/src/data/simple_item.dart';
+export 'package:dialogs_x/src/dialogs.dart';
+export 'package:dialogs_x/src/widgets.dart';
+export 'package:dialogs_x/src/res/styles.dart';
+export 'package:dialogs_x/src/extensions_functions.dart';
+export 'package:dialogs_x/src/props/dialog_text_field.dart';
+export 'package:dialogs_x/src/props/progress_dialog_type.dart';
+
+export 'package:dialogs_x/src/screens/single_selection_screen.dart';
+export 'package:dialogs_x/src/screens/multi_selection_screen.dart';
